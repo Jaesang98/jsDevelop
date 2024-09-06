df146057c9b3b5b3242efe5a0bd6d1d971d0b034
jsnam <jsnam@mcnc.co.kr>
2024-09-06 09:23:14 +0900

    Revert "Add files via upload"
    This reverts commit 7034c80f7a21d5e3e21fe2fee3ae797c47ae6d74.
    
    Revert "jsnam 모멘트 완료"
    
    This reverts commit c8d73600bdeb5c678558c3148c4123c680fe2ca1.

--- src/router/routes/codeLibraryRouter.js
+++ src/router/routes/codeLibraryRouter.js
@@ -5,17 +5,5 @@ const codeRoutes = [
         props: true,
         component: () => import(/* webpackChunkName: "CODE" */ '@/views/CODEPAGE/LIBRARY/MOMENT/MOMENT_1.vue')
     },
-    {
-        path: '/MOMENT_2',
-        name: 'MOMENT_2',
-        props: true,
-        component: () => import(/* webpackChunkName: "CODE" */ '@/views/CODEPAGE/LIBRARY/MOMENT/MOMENT_2.vue')
-    },
-    {
-        path: '/MOMENT_3',
-        name: 'MOMENT_3',
-        props: true,
-        component: () => import(/* webpackChunkName: "CODE" */ '@/views/CODEPAGE/LIBRARY/MOMENT/MOMENT_3.vue')
-    },
 ];
 export default codeRoutes;
